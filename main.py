0863a7eef46023189d475e7ed73decfe91cb8a79
CyberAvian <cyberavian025@gmail.com>
2021-02-23 20:26:52 -0500

    Linked Moody to Spotify script

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import os
 import logging
 import random
+import moods
 
 client = discord.Client()
 moody_token = os.getenv("MOODY_TOKEN")
@@ -24,19 +25,33 @@ async def on_message(message):
 
   message_content = message.content.lower()
   logging.info(f"Message: {message_content}")
+
   if message_content.startswith("moody,"):
     for trigger in triggers:
       logging.info(f"Trigger: {trigger}")
+      
       if trigger in message_content:
-        response_num = random.randint(0, len(triggers)-1)
         logging.info(f"{trigger} found in {message_content}")
-        await message.channel.send(triggers[trigger][response_num])
+        response_num = random.randint(0, len(triggers)-1)
+        
+        # The hello trigger does not generate a song
+        if trigger == 'hello':
+          logging.info(f"{trigger} does not have a song")
+          response = triggers[trigger][response_num]
+        else:
+          logging.info(f"Fetching {trigger} song")
+          song_info = moods.convert_song_to_string(trigger)
+          response = f"{triggers[trigger][response_num]}\n\n{song_info}"
+
+        await message.channel.send(response)
         return
       else:
         logging.info(f"{trigger} not found in {message_content}")
+    
     logging.info("Did not find any triggers.")
     await message.channel.send("I do not understand the response. Please try again.")
   else:
+    logging.info(f"{message_content} does not start with moody,")
     return
 
 client.run(moody_token)
